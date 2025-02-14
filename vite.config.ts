49c974f7e8d7bf26f6f007c72b5edf7426a1c943
devonyura <rnidevon@gmail.com>
2025-02-14 10:29:19 +0800

    update vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,16 @@ export default defineConfig({
   optimizeDeps: {
     exclude: [`@ionic/pwa-elements/loader`],
   },
+	base: '/crud-siswa/',
+	build: {
+		rollupOptions: {
+			output: {
+				entryFileNames: `assets/[name]-[hash].js`,
+				chunkFileNames: `assets/[name]-[hash].js`,
+				assetFileNames: `assets/[name]-[hash].[ext]`
+			}
+		}
+	},
   plugins: [
     react(),
     legacy(),
