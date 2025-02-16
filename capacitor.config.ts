7d2f2dbc6a78613d34fd8cc29033482b4d1e3aef
devonyura <rnidevon@gmail.com>
2025-02-16 18:00:53 +0800

    add splash screen

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -9,6 +9,17 @@ const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
   appName: 'CRUD SISWA',
   webDir: 'dist',
+  plugins: {
+		SplashScreen: {
+			launchShowDuration: 3000, // Durasi splash screen dalam ms (3 detik)
+			launchAutoHide: true, // Sembunyikan otomatis setelah durasi
+			androidScaleType: "CENTER_CROP", // Sesuaikan posisi gambar di Android
+			showSpinner: false, // Matikan spinner default
+			backgroundColor: "#ffffff", // Warna latar belakang
+			androidSplashResourceName: "splash", // Nama gambar splash di Android
+			iosSplashResourceName: "splash", // Nama gambar splash di iOS
+		},
+	},
 };
 
 export default config;
